be42de2fa01445789e77464718d887a6ceae1cba
nilarte <nilart@gmail.com>
2022-06-26 13:57:22 +0530

    Update Class_Method_and_Attr_Demo.py

--- Class_Method_and_Attr_Demo.py
+++ Class_Method_and_Attr_Demo.py
@@ -2,19 +2,19 @@ class ParentTestCase:
     destination = "localhost"
     @classmethod
     def setUpClass(cls):
-        print(ParentTestCase.destination)
+        print(cls.destination)
 
 
 class ChildTestCase1(ParentTestCase):
     @classmethod
     def setUpClass(cls):
-        ParentTestCase.destination = "google.com"
+        cls.destination = "google.com"
         super().setUpClass()
 
 class ChildTestCase2(ParentTestCase):
     @classmethod
     def setUpClass(cls):
-        ParentTestCase.destination = "stackoverflow.com"
+        cls.destination = "stackoverflow.com"
         super().setUpClass()
 
 ChildTestCase1.setUpClass()
